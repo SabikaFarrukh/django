80b232665c219c55380f95d55711e398d4fa6e92
SabikaFarrukh <110960972+SabikaFarrukh@users.noreply.github.com>
2022-08-10 15:03:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+
 import requests
 from bs4 import BeautifulSoup
 url = "https://www.techtarget.com/searchenterpriseai/definition/AI-Artificial-Intelligence"
 r = requests.get(url)
 htmlContent = r.content
-#print(htmlContent)
+print(htmlContent)
 soup = BeautifulSoup(htmlContent, 'html.parser')
 
 markup ="<p><!---this is a coment--></p>"
@@ -11,14 +12,17 @@ soup2 = BeautifulSoup(markup)
 print(type(soup2.p.string))
 
 
-#print(soup.prettify)
+print(soup.prettify)
 title = soup.title
+print(title)
+print(type(title))
+print(type(title.string))
+paras=soup.find_all('p')
+print(paras)
+anchors=soup.find_all('a')
+print(anchors)
 
-#print(paras)
-#print(anchors)
 print(soup.find('p') )
-print(soup.find('p')['class'])
-print(soup.find_all("p",class_="lead"))
 anchors = soup.find_all('a')
 all_links = set()
 for link in anchors:
@@ -27,6 +31,9 @@ for link in anchors:
          all_links.add (link)
          print(linktext)
 
-
-
 print(soup. get_text())
+
+navlinksTechtargetNetwork= soup.find("nav-links-TechtargetNetwork")
+print(navlinksTechtargetNetwork)
+for elem in navlinksTechtargetNetwork.children:
+    print(elem)
